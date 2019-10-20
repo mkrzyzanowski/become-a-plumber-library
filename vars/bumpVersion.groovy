0377df7e4daafd2e14af05501131982a3b5ca7aa
Michal <mich.krzyzanowski@gmail.com>
2019-10-21 00:29:37 +0200

    bumpVersion step

--- vars/bumpVersion.groovy
+++ vars/bumpVersion.groovy
@@ -1,5 +1,5 @@
 def call(Map cfg) {
     def version = readFile cfg.file ?: 'VERSION'
     def (major, minor, bugfix) = version.tokenize('.')
-    env.newVersion = "$major.$minor.${++(bugfix as Integer)}"
+    env.newVersion = "$major.$minor.${(bugfix as Integer)+1}"
 }
